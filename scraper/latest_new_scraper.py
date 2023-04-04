b9264e2d3979efbd9b499e997433ebe6b4315b44
MacroMan5 <119378668+MacroMan5@users.noreply.github.com>
2023-04-04 12:31:26 -0400

    Updated .gitignore

--- scraper/latest_new_scraper.py
+++ scraper/latest_new_scraper.py
@@ -42,13 +42,45 @@ def scrape_website(website):
 
 
     return urls #return the list of all the url found on the websites
-
+#testing the function scrape_website
 url_list = []
 for website in websites:
     url_list = scrape_website(website)
     for i in url_list: # print the list of urls skip line for each url in the 
         print(i)
 
+
+def scrape_article(url, selectors):
+    all_article = []
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+    }
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+    except requests.RequestException as e:
+        print(f"Error scraping {url}: {str(e)}")
+        return None
+
+    soup = BeautifulSoup(response.content, "lxml")
+
+    title_element = soup.select_one(selectors["title"])
+    title = title_element.text.strip() if title_element else "No title found"
+
+    content_element = soup.select_one(selectors["content"])
+    content = content_element.text.strip() if content_element else "No content found"
+
+    # Assuming that the date selector is added to the selectors dictionary
+    date_element = soup.select_one(selectors["date"])
+    date_text = date_element.text.strip() if date_element else "No date found"
+    try:
+        date = datetime.strptime(date_text, "%Y-%m-%d")
+    except ValueError:
+        date = None
+
+    all_article.append({"title": title, "content": content, "date": date})
+
+    return all_article
 
 
 
